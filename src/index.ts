72a237fd942175ac96c1fa1bcc53ec23456148a3
kecy <keishiro.oyama@kecy.me>
2022-04-14 09:06:51 +0900

    add type definition for zenhub events

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
+type ZenHubEventType = "issue_transfer" | "estimate_set" | "estimate_cleared" | "issue_reprioritized";
+
+interface ZenHubEvent {
+  type: ZenHubEventType;
+  githubUrl: string;
+  organization: string;
+  repo: string;
+  userName: string;
+  issueNumber: number;
+  issueTitle: string;
+}
+
+type ZenHubIssueTransferEvent = ZenHubEvent & {
+  toPipelineName: string;
+  fromPipelineName: string;
+  workspaceId: string;
+  workspaceName: string;
+}
+
+type ZenHubEstimateSetEvent = ZenHubEvent & {
+  estimate: number;
+}
+
+type ZenHubEstimateClearedEvent = ZenHubEvent;
+
+type ZenHubIssueReprioritizedEvent = ZenHubEvent & {
+  toPipelineName: string;
+  fromPosition: number;
+  toPosition: number;
+  workspaceId: string;
+  workspaceName: string;
+};
+
 function Main(): void {
   Logger.log(HelloWorld());
 }
